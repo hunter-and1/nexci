d1a46249170d7cdfbd0597f56ec07bbaa9f73ffc
= <abdellah.elkadiri.dev@gmail.com>
2021-01-29 20:51:16 +0100

    fix bug return_bytes

--- app/Helpers/app_helper.php
+++ app/Helpers/app_helper.php
@@ -44,6 +44,17 @@ if (! function_exists('nex_max_file_upload_in_bytes'))
 {	
 	function nex_max_file_upload_in_bytes()
 	{
+        function return_bytes ($size_str)
+        {
+            switch (substr ($size_str, -1))
+            {
+                case 'M': case 'm': return (int)$size_str * 1048576;
+                case 'K': case 'k': return (int)$size_str * 1024;
+                case 'G': case 'g': return (int)$size_str * 1073741824;
+                default: return $size_str;
+            }
+        }
+
 		// Select maximum upload size
 		$max_upload = return_bytes(ini_get('upload_max_filesize'));
 
